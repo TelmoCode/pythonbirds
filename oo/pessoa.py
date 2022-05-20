629e5ee74b33cc0297779188ef160f20cd067c41
Telmo Goncalves <telmogferreira@gmail.com>
2022-05-20 00:15:20 -0300

    criação do metodo inicial nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,13 +2,19 @@
 
 
 class Pessoa:
+
+    def __init__(self, nome = None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-
+    p = Pessoa('Angelina')
     print(Pessoa.cumprimentar(p))
     print(dir(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Telmo'
+    print(p.nome)
 
